98975e703b5c0ad8193215a23c9cbd8c4febaeca
SAURABH NEGI <saurabhnegi.35@gmail.com>
2022-12-03 20:05:02 +0530

    setup static files and access for layouts and pages

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+//Extract Styles and Scripts from Sub Pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
 
 const port = 8000;
 
@@ -19,26 +29,3 @@ app.listen(port, function(err) {
 
     console.log(`Server running on port: ${port}`);
 });
-
-
-
-
-// const express = require('express');
-// const app = express();
-// const port = 8000;
-
-// // use express router
-// app.use('/', require('./routes'));
-
-// // set up the view engine
-// app.set('view engine', 'ejs');
-// app.set('views', './views');
-
-
-// app.listen(port, function(err){
-//     if (err){
-//         console.log(`Error in running the server: ${err}`);
-//     }
-
-//     console.log(`Server is running on port: ${port}`);
-// });
