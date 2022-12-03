efff678b406262a72fdf71d4127b285fb105594c
SAURABH NEGI <saurabhnegi.35@gmail.com>
2022-12-03 20:14:53 +0530

    Installed Mongoose and Set it Up

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 app.use(express.static('./assets'));
 
