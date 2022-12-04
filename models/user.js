e1197fd3db7c83b85eb84f32b4df79e99dcdcbc2
SAURABH NEGI <saurabhnegi.35@gmail.com>
2022-12-04 10:38:05 +0530

    Setup Routes of User Login and Signup

---
+++ models/user.js
@@ -0,0 +1,25 @@
+//Import Mongoose
+const mongoose = require('mongoose');
+
+// Create the Schema for User Login
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+moddule.exports = User;
