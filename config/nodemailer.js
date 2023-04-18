aec24c4dee718abb35c32fc84c3b0078364cbaab
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-04-18 22:13:22 +0530

    Added Nodemailer and Google Oauth

---
+++ config/nodemailer.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+const env = require("./environment");
+const ejs = require("ejs");
+const path = require("path");
+
+let transporter = nodemailer.createTransport(env.smtp);
+
+let renderTemplate = (data, relativePath) => {
+  let mailHTML;
+  ejs.renderFile(
+    path.join(__dirname, "../views/mailers", relativePath),
+    data,
+    function (err, template) {
+      if (err) {
+        console.log("error in rendering template", err);
+        return;
+      }
+
+      mailHTML = template;
+    }
+  );
+
+  return mailHTML;
+};
+
+module.exports = {
+  transporter: transporter,
+  renderTemplate: renderTemplate,
+};
